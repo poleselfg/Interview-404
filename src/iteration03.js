fafe8760888eadde9ea3cc93e9b568f8e9d19e7e
Pole <poleselfg@gmail.com>
2020-08-22 02:21:58 -0300

    iteration03

--- src/iteration03.js
+++ src/iteration03.js
@@ -4,4 +4,26 @@ When quality reaches 0 the store will have to remove the book from it's inventor
 
 const json = require(`../data/data.json`);
 const fs = require("fs");
+const moment = require("moment");
+const now = moment(new Date());
 
+const diffBook = (bookName) => {
+  const updated = json.data.map((item) => {
+    if (bookName == item.titulo) {
+      const diffTime = moment.duration(now.diff(item.date));
+      const diffDays = diffTime.asDays();
+      item.quality = item.quality -= diffDays;
+    }
+    if (item.quality <= 0) {
+      item.cantidad = 0;
+    }
+    return item;
+  });
+
+  json.data = updated;
+
+  fs.writeFileSync("data/data.json", JSON.stringify(json));
+};
+
+diffBook("Rayuela");
+diffBook("El Aleph");
