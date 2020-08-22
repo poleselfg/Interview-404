485a994a2983d1772e063700ec796f9f8c977801
Pole <poleselfg@gmail.com>
2020-08-22 00:29:16 -0300

    iteration 2

--- src/iteration02.js
+++ src/iteration02.js
@@ -14,3 +14,40 @@ Feature: Rent a book
         Given there are three units of a book titled "Rayuela" in the store
         When a customer returns a book titled "Rayuela"
         Then the store has four units of a book titled "Rayuela" */
+
+const json = require(`../data/data.json`);
+const fs = require("fs");
+
+const rentBook = (bookName) => {
+  const updated = json.data.map((item) => {
+    if (bookName == item.titulo && item.cantidad > 0) {
+      item.cantidad -= 1;
+      console.log(`${bookName} alquilado restan ${item.cantidad} en deposito`);
+    }
+
+    return item;
+  });
+
+  json.data = updated;
+
+  fs.writeFileSync(`../data/data.json`, JSON.stringify(json));
+};
+
+const returnBook = (bookName) => {
+  const updated = json.data.map((item) => {
+    if (bookName == item.titulo && item.cantidad > 0) {
+      item.cantidad += 1;
+      console.log(`${bookName} devuelto restan ${item.cantidad} en deposito`);
+    }
+
+    return item;
+  });
+
+  json.data = updated;
+
+  fs.writeFileSync(`../data/data.json`, JSON.stringify(json));
+};
+rentBook("Rayuela");
+rentBook("El Aleph");
+returnBook("Rayuela");
+returnBook("El Aleph");
